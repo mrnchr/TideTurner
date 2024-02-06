42a6bf639f07a6eabbc8d026c1eacd093d522770
Kirill <k.d.kuznetsov@stud.mospolytech.ru>
2024-02-06 14:59:29 +0300

    22.0 tried fix webGL and fix barrel

--- Assets/Scripts/Level/FloatingObject/Barrel.cs
+++ Assets/Scripts/Level/FloatingObject/Barrel.cs
@@ -29,8 +29,8 @@ public class Barrel : MonoBehaviour, ILevelUpdatable, IUpdatable
         rb.angularVelocity = 0;
         rb.velocity = Vector2.zero;
         transform.eulerAngles = new Vector3(0, 0, 0);
-        boomEffect.transform.parent = transform;
-        boomEffect.transform.position = Vector3.zero;
+        //.transform.parent = transform;
+        boomEffect.transform.localPosition = Vector3.zero;
     }
 
     private void SubsribeToObstacle()

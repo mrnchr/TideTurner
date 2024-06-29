cf6ab878b954e4a6005674a80f560b8497874052
Subbota Mikhail <mihail.subbota@inbox.ru>
2024-06-29 14:10:15 +0300

    feat: Add buoy graphic

---
+++ Assets/Muchachos/TideTurner/Runtime/Level/Buoy/BuoySwitcher.cs
@@ -0,0 +1,50 @@
+﻿using Muchachos.TideTurner.Runtime.Level.Savings;
+using Muchachos.TideTurner.Runtime.Physics;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+namespace Muchachos.TideTurner.Runtime.Level.Buoy
+{
+    public class BuoySwitcher : MonoBehaviour
+    {
+        private static readonly int _float = Animator.StringToHash("Float");
+
+        [SerializeField]
+        private Light2D _light;
+
+        [SerializeField]
+        private FloatingBody _body;
+
+        [SerializeField]
+        private Animator _animator;
+
+        [SerializeField]
+        private Collider2D _idleCollider;
+
+        [SerializeField]
+        private Collider2D _floatCollider;
+
+        [SerializeField]
+        private CheckPoint _checkPoints;
+
+        private void Start()
+        {
+            _light.enabled = false;
+            _body.enabled = false;
+            _idleCollider.enabled = true;
+            _floatCollider.enabled = false;
+        }
+
+        private void Update()
+        {
+            if (_checkPoints.IsChecked)
+            {
+                _light.enabled = true;
+                _body.enabled = true;
+                _idleCollider.enabled = false;
+                _floatCollider.enabled = true;
+                _animator.SetBool(_float, true);
+            }
+        }
+    }
+}

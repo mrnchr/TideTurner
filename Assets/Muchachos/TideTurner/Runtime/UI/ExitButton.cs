2098cd988d6e9b44b750a505cea85709eff4c5e0
Kirill <k.d.kuznetsov@stud.mospolytech.ru>
2024-08-25 14:12:04 +0300

    Ya.Int 3.0

--- Assets/Muchachos/TideTurner/Runtime/UI/ExitButton.cs
+++ Assets/Muchachos/TideTurner/Runtime/UI/ExitButton.cs
@@ -4,6 +4,12 @@ namespace Muchachos.TideTurner.Runtime.UI
 {
     public class ExitButton : MonoBehaviour
     {
+        private void Awake()
+        {
+            if (Application.isMobilePlatform)
+                gameObject.SetActive(false);
+        }
+
         public void Exit()
         {
             Application.Quit();
